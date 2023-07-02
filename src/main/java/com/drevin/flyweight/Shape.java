53a4e9e410b4d661b6e333e219f1c96039c9c019
Drevinx <121133400+Drevinx@users.noreply.github.com>
2023-07-02 11:10:19 +0200

    Flyweight

---
+++ src/main/java/com/drevin/flyweight/Shape.java
@@ -0,0 +1,12 @@
+package com.drevin.flyweight;
+
+public abstract class Shape {
+
+    public void draw(int radius, String color, String borderColor)   {
+
+    }
+
+    public void draw(int lenght, int breadth, String fillStyle){
+
+    }
+}
