74c2c376c985347f850a68d205db54e002bd4e82
Drevinx <121133400+Drevinx@users.noreply.github.com>
2023-07-06 19:26:38 +0200

    Builder

---
+++ src/main/java/com/drevin/creational/builder/Test.java
@@ -0,0 +1,14 @@
+package com.drevin.creational.builder;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        HttpClient httpClient = new HttpClient("GET", "https:/test.com",null,null,null,null);
+
+        HttpClient.HttpClientBuilder httpClientBuilder = new HttpClient.HttpClientBuilder();
+        httpClientBuilder.url("test.com");
+        httpClient = httpClientBuilder.build();
+        System.out.println(httpClient);
+    }
+}
